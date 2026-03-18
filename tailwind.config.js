8356ff48482a876cdb42971c8cdfd06be109c53c
temidaradev <temidaradev@proton.me>
2026-03-18 08:49:22 +0300

    special kettek16 theme and theme selector visual issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,31 +16,38 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: 'var(--color-black)',
-        white: 'var(--color-white)',
+        black: "var(--color-black)",
+        white: "var(--color-white)",
         slate: {
-          400: 'var(--color-slate-400)',
-          500: 'var(--color-slate-500)',
+          400: "var(--color-slate-400)",
+          500: "var(--color-slate-500)",
         },
         green: {
-          500: 'var(--color-green-500)',
+          400: "var(--color-green-400)",
+          500: "var(--color-green-500)",
         },
         indigo: {
-          400: 'var(--color-indigo-400)',
-          500: 'var(--color-indigo-500)',
-          600: 'var(--color-indigo-600)',
-          900: 'var(--color-indigo-900)',
+          400: "var(--color-indigo-400)",
+          500: "var(--color-indigo-500)",
+          600: "var(--color-indigo-600)",
+          900: "var(--color-indigo-900)",
         },
         purple: {
-          600: 'var(--color-purple-600)',
-          700: 'var(--color-purple-700)',
+          400: "var(--color-purple-400)",
+          500: "var(--color-purple-500)",
+          600: "var(--color-purple-600)",
+          700: "var(--color-purple-700)",
         },
 
         red: {
-          400: 'var(--color-red-400)',
+          400: "var(--color-red-400)",
+          500: "var(--color-red-500)",
+        },
+        orange: {
+          500: "var(--color-orange-500)",
         },
         neutral: {
-          900: 'var(--color-neutral-900)',
+          900: "var(--color-neutral-900)",
         },
       },
     },
