3b58b78b3f6e2b0a5f7c85a8aa3f170c86f5610b
yofukashino <73281112+yofukashino@users.noreply.github.com>
2026-08-06 06:10:36 +0530

    feat: autogen dixous and librusty sources

--- packaging/flatpak/generate-sources.sh
+++ packaging/flatpak/generate-sources.sh
@@ -28,60 +28,57 @@ pip install flatpak-cargo-generator
 
 flatpak-cargo-generator "$CUR_DIR/Cargo.lock" -o "cargo-sources.json"
 
-#dioxus_releases=$(curl -fsSL "https://api.github.com/repos/DioxusLabs/dioxus/releases/latest")
+make_asset_source() {
+    releases=$(curl -fsSL "$1")
+    {
+        echo "["
+        suffix=","
+        for arch in x86_64 aarch64; do
+            file="${2//\{\@\}/$arch}"      
+            asset=$(jq -c --arg file "$file" '.assets[] | select(.name == $file)' <<<"$releases")
+            url=$(jq -r '.browser_download_url' <<<"$asset")
+            digest=$(jq -r '.digest' <<<"$asset")
+            if [[ "$digest" != "null" ]]; then
+                algo=${digest%%:*}
+                hash=${digest#*:}    
+            else 
+                algo="sha512"
+                curl -L -o "$file" "$url"
+                hash=$(sha512sum "$file" | awk '{print $1}')
+            fi           
+            cat <<EOF
+  {
+    "type": "$3",
+    "url": "$url",
+    "$algo": "$hash",
+    "$4": "$5",
+    "only-arches": ["$arch"]
+  }$suffix
+EOF
+        suffix=""
+        done
+        echo "]"
 
-#{
-#    echo "["
-#    suffix=","
-#    for arch in x86_64 aarch64; do
-#        file="dx-${arch}-unknown-linux-gnu.zip"
-#        asset=$(jq -c --arg file "$file" '.assets[] | select(.name == $file)' <<<"$dioxus_releases")
-#        url=$(jq -r '.browser_download_url' <<<"$asset")
-#        digest=$(jq -r '.digest' <<<"$asset")
-#        algo=${digest%%:*}
-#        hash=${digest#*:}       
-#        cat <<EOF
-#  {
-#    "type": "archive",
-#    "url": "$url",
-#    "$algo": "$hash",
-#    "dest": "dioxus-cli",
-#    "only-arches": ["$arch"]
-#  }$suffix
-#EOF
-#    suffix=""
-#    done
-#    echo "]"
-#} > dioxus-cli.json
+    } > "$6"
+}
 
-# latest doesnt work so pinned to v130.0.7
+make_asset_source \
+"https://api.github.com/repos/denoland/rusty_v8/releases/tags/v$(grep -oPm1 '"dest": "cargo/vendor/v8-\K[0-9]+([\.\d]+)' cargo-sources.json)" \
+"librusty_v8_release_{@}-unknown-linux-gnu.a.gz" \
+"file" \
+"dest-filename" \
+"librusty.a.gz" \
+"librusty.json"
 
-#rusty_v8_releases=$(curl -fsSL "https://api.github.com/repos/denoland/rusty_v8/releases/latest")
+make_asset_source \
+"https://api.github.com/repos/DioxusLabs/dioxus/releases/tags/v$(grep -oPm1 'cargo install dioxus-cli@\K[0-9]+([\.\d]+)' $CUR_DIR/.github/workflows/release.yml)" \
+"dx-{@}-unknown-linux-gnu.zip" \
+"archive" \
+"dest" \
+"dioxus-cli" \
+"dioxus-cli.json"
 
-#{
-#    echo "["
-#    suffix=","
-#    for arch in x86_64 aarch64; do
-#        file="librusty_v8_release_${arch}-unknown-linux-gnu.a.gz"        
-#        asset=$(jq -c --arg file "$file" '.assets[] | select(.name == $file)' <<<"$rusty_v8_releases")
-#        url=$(jq -r '.browser_download_url' <<<"$asset")
-#        digest=$(jq -r '.digest' <<<"$asset")
-#        algo=${digest%%:*}
-#        hash=${digest#*:}       
-#        cat <<EOF
-#  {
-#    "type": "file",
-#    "url": "$url",
-#    "$algo": "$hash",
-#    "dest-filename": "librusty.a.gz",
-#    "only-arches": ["$arch"]
-#  }$suffix
-#EOF
-#    suffix=""
-#    done
-#    echo "]"
 
-#} > librusty.json
 
 cp "pnpm-sources.json" "$CUR_DIR/packaging/flatpak/pnpm-sources.json"
 
@@ -89,4 +86,4 @@ cp "cargo-sources.json" "$CUR_DIR/packaging/flatpak/cargo-sources.json"
 
 cp "dioxus-cli.json" "$CUR_DIR/packaging/flatpak/dioxus-cli.json"
 
-#cp "librusty.json" "$CUR_DIR/packaging/flatpak/librusty.json"
+cp "librusty.json" "$CUR_DIR/packaging/flatpak/librusty.json"
